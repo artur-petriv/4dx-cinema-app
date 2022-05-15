606c75069ceef9f7532a4ec10e4c72b52fc396b0
Artur Petriv <artur.petriv@gmail.com>
2022-05-15 15:22:26 +0300

    update

--- src/components/Film/Genres.jsx
+++ src/components/Film/Genres.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
-const genres = ['боевик', 'ужасы', 'фантастика'];
+const genres = ["боевик", "ужасы", "фантастика"];
 
 export default function FilmGenres() {
-	return (
+  return (
     <GenresParagraph>
+      <strong>Жанри:{` `}</strong>
       {genres?.map((genre, index) => (!index ? genre : `, ${genre}`))}
     </GenresParagraph>
   );
--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,74 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
-import Container from '../components/Container';
-import FilmGenres from '../components/Film/Genres';
-import FilmTitle from '../components/Film/Title';
-import Poster from '../components/Poster';
-import FilmRating from '../components/Film/Rating';
-import FilmDatepicker from '../components/Film/Datepicker';
-import Filter from '../components/Filter';
-import FilmTicket from '../components/Film/Ticket';
-import FilmHall from '../components/Film/Hall';
+import React from "react";
+import styled from "styled-components";
+import Container from "../components/Container";
+import FilmGenres from "../components/Film/Genres";
+import FilmTitle from "../components/Film/Title";
+import Poster from "../components/Poster";
+import FilmRating from "../components/Film/Rating";
+import FilmDatepicker from "../components/Film/Datepicker";
+import Filter from "../components/Filter";
+import FilmTicket from "../components/Film/Ticket";
+import FilmHall from "../components/Film/Hall";
 import { useParams } from "react-router-dom";
-import { fetchOneFilm } from '../http/filmAPI';
+import { fetchOneFilm } from "../http/filmAPI";
 
 const FiltersData = [
   {
-    name: 'Формат',
-    type: 'select',
+    name: "Формат",
+    type: "select",
     options: [
-      { name: '4DX', value: '4dx' },
-      { name: '3D', value: '3d' },
-      { name: '2D', value: '2d' },
+      { name: "4DX", value: "4dx" },
+      { name: "3D", value: "3d" },
+      { name: "2D", value: "2d" },
     ],
   },
   {
-    name: 'Время',
-    type: 'select',
+    name: "Время",
+    type: "select",
     options: [
-      { name: '11:45', value: '11:45' },
-      { name: '13:00', value: '13:00' },
-      { name: '17:45', value: '17:45' },
+      { name: "11:45", value: "11:45" },
+      { name: "13:00", value: "13:00" },
+      { name: "17:45", value: "17:45" },
     ],
   },
 ];
-
-// const film = {
-//   id: 1,
-//   name: "Веном",
-//   year: "2022",
-//   country: "США",
-//   language: "Украинский",
-//   img: "",
-//   trailer: "",
-//   duration: 126,
-//   imgUrl: "https://razborkatesla.com.ua/o/card.jpg",
-//   rating: "7.2",
-//   genres: [
-//     {
-//       id: 1,
-//       title: "Фентези",
-//       value: "fantasy",
-//       createdAt: "2022-07-31T21:00:00.000Z",
-//       updatedAt: "2022-07-31T21:00:00.000Z",
-//     },
-//     {
-//       id: 3,
-//       title: "Трилер",
-//       value: "thriller",
-//       createdAt: "2022-01-07T22:00:00.000Z",
-//       updatedAt: "2022-01-07T22:00:00.000Z",
-//     },
-//   ],
-// };
 
 export default function Film() {
   const { id } = useParams();
   const [film, setFilm] = React.useState({});
 
   React.useEffect(() => {
-    fetchOneFilm(id).then(data => setFilm(data));
+    fetchOneFilm(id).then((data) => setFilm(data));
   }, []);
+
+  console.log({ ...film });
 
   return (
     <FilmSection>
@@ -78,22 +51,41 @@ export default function Film() {
             <Poster imageUrl={film.img} />
             <FilmInfo>
               <FilmTitle name={film.name} />
+              <FilmRating rating={film.rating} />
               <FilmGenres />
-              <FilmRating />
+              <div style={{ marginTop: "4px" }}>
+                <strong>Рік:</strong> 2022
+              </div>
+              <div style={{ marginTop: "4px" }}>
+                <strong>Країна:</strong> США
+              </div>
+              <div style={{ marginTop: "4px" }}>
+                <strong>Мова:</strong> Український (дубляж)
+              </div>
+              <div style={{ marginTop: "4px" }}>
+                <strong>Тривалість:</strong> 128 хв.
+              </div>
+              <div style={{ marginTop: "4px" }}>
+                <strong>Вікове обмеження:</strong> 16+
+              </div>
+              <div style={{ marginTop: "4px" }}>
+                <strong>Прокат:</strong> з 20 червня до 16 липня
+              </div>
             </FilmInfo>
           </FilmCard>
           <FilmTrailer>
             <iframe
               style={{
-                width: '100%',
-                height: '360px',
-                borderRadius: 'var(--border-radius-medium)',
+                width: "100%",
+                height: "360px",
+                borderRadius: "var(--border-radius-medium)",
               }}
               src="https://www.youtube.com/embed/Othd8W8o3t0"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-              allowFullScreen></iframe>
+              allowFullScreen
+            ></iframe>
           </FilmTrailer>
         </FilmHead>
 
@@ -163,7 +155,7 @@ const PosterImage = styled.div``;
 
 const FilmMain = styled.div`
   display: grid;
-  grid-template-areas: 'ticket ticket ticket hall hall hall hall hall hall hall hall hall';
+  grid-template-areas: "ticket ticket ticket hall hall hall hall hall hall hall hall hall";
   gap: 32px;
 `;
 
